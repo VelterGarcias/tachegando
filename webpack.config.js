805ee9399642d485cf248c42cfa0dc91a95364f5
VelterGarcias <veltj2@gmail.com>
2021-06-23 09:46:25 -0300

    pronto

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    // mode: 'production',
+    // mode: 'development',
+    mode: 'production',
     entry: './public/assets/scripts/index.js',
     output: {
         filename: 'bundle.js',
