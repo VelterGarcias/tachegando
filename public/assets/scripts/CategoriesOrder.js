6c9da5db800a769dd99172e397317d3625d209aa
VelterGarcias <veltj2@gmail.com>
2022-01-26 10:40:49 -0300

    antes de instalar sorteableJS

--- public/assets/scripts/CategoriesOrder.js
+++ public/assets/scripts/CategoriesOrder.js
@@ -6,7 +6,9 @@ import {
   getFormValues,
   setFormValues,
   getQueryString,
-  showModal
+  showModal,
+  onSnapshotError,
+  appendTemplate
 } from "./utils";
 
 //======================== Atualizar Dados ====================================
@@ -19,7 +21,9 @@ document.querySelectorAll("#form-categories").forEach(async (form) => {
   const btnSubmit = form.querySelector("[type=submit]");
 
   const productDate = [];
-  
+
+  const companyData = {}
+  // companyData.categoriesOrder = ["Sorveteria", "Lanches" ];
 
   // new IMask(inputPhone, {
   //   mask: "(00) [0]0000-0000)",
@@ -30,27 +34,102 @@ document.querySelectorAll("#form-categories").forEach(async (form) => {
   // });
 
 
+  const renderCategoriesOrder = (targetElement, productOptions) => {
+
+    // ========================
+    
+    targetElement.innerHTML = "";
+    // const categoryWrapper = document.querySelector('div.categories')
+    // categoryWrapper.innerHTML = "";
+    // // console.log("productOptions", productOptions);
+  
+    let categories = [...new Set(productOptions.map(item => item.category))];
+    console.log("categories", categories);
+  
+    // const wrapMenuCategories = document.querySelector('#menu-categories')
+
+    if (!companyData.hasOwnProperty('categoriesOrder')) {
+      categories.sort(function(a, b){
+          if(a < b) { return -1; }
+          if(a > b) { return 1; }
+          return 0;
+      })
+    } else {
+      categories = Array.from(new Set(companyData.categoriesOrder.concat(categories)));
+      // console.log("allCategoriesOrdened", allCategoriesOrdened);
+    }
+    
+
+    // console.log("categories-sorted", categories);
+    // const objCategory = {...categories};
+    // console.log("categories-obj", objCategory);
+    // const array2 = ["teste", "teste2", "Lanches", "Sorveteria"];
+    // console.log(Array.from(new Set(categories.concat(array2))));
+    
+    
+    categories.forEach((category, indexCategory, array) => {
+      
+  
+      appendTemplate(
+        targetElement,
+        `li draggable="true"`,
+        category
+      );
+
+  
+    })
+  
+    
+  
+    
+  };
+
+
+
   auth.onAuthStateChanged(async (user) => {
     console.log(user);
     if (user) {
       userGlobal = user;
 
-      const products = await db
-        .collection("products")
-        .where("companyId", "==", user.uid)
-        .get();
+      // const products = await db
+      //   .collection("products")
+      //   .where("companyId", "==", user.uid)
+      //   .get();
 
        
-      products.forEach((item) => {
+      // products.forEach((item) => {
         
-        const product = item.data();
-        if (product.is_online) {
-          product.id = item.id;
-          productDate.push(product);
-        }
+      //   const product = item.data();
+      //   if (product.is_online) {
+      //     product.id = item.id;
+      //     productDate.push(product);
+      //   }
         
-      });
-      console.log("product", productDate);
+      // });
+      // console.log("product", productDate);
+
+      const products = [];
+      db.collection("products")
+        .where("companyId", "==", user.uid)
+        .onSnapshot((getProducts) => {
+          products.length = 0;
+          
+          getProducts.forEach((product) => {
+            const productData = product.data();
+            productData.firebaseId = product.id;
+            products.push(productData);
+          });
+          
+          // page.querySelector('p').innerHTML = products[0].name
+          console.log(products);
+
+          const ulProducts = document.querySelector(".sorteable-ul");
+          renderCategoriesOrder(ulProducts, products);
+
+          // renderProductsAdmin2(page, products);
+
+
+        }, onSnapshotError);
         
     } else {
       auth.signOut();
@@ -99,7 +178,10 @@ document.querySelectorAll("#form-categories").forEach(async (form) => {
   });
 
   document.addEventListener('dragenter', function(event) {
-      event.target.style['border-bottom'] = 'solid 4px blue';
+      if (event.target.closest('ul.sorteable-ul')) {
+        event.target.style['border-bottom'] = 'solid 4px blue';
+      }
+      
   });
 
   document.addEventListener('dragleave', function(event) {
@@ -108,8 +190,11 @@ document.querySelectorAll("#form-categories").forEach(async (form) => {
 
   document.addEventListener('drop', function(event) {
       event.preventDefault();
-      event.target.style['border-bottom'] = '';
-      event.target.parentNode.insertBefore(dragging, event.target.nextSibling);
+      // console.log(event.target);
+      if (event.target.closest('ul.sorteable-ul')) {
+        event.target.style['border-bottom'] = '';
+        event.target.parentNode.insertBefore(dragging, event.target.nextSibling);
+      }
   });
 
 
